7bb8c5c2be44d480f573364ab8f32946447e9a0c
Nahid Monowar <nahidmonowar2003@gmail.com>
2026-08-13 20:18:13 +0600

    Implement odd/even check in multiplybyoddeven.cpp

---
+++ multiplybyoddeven.cpp
@@ -0,0 +1,12 @@
+#include<bits/ctdc++.h>
+using namespace std;
+int main ( ) {
+  int a = 5;
+  int result = (a&1);
+  if(result == 1) {
+    cout << "Odd";
+  }
+  else {
+  cout << "Even";
+  }
+)
